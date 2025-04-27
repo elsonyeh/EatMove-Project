c72440d6ea435ccf6aada3e5305a16dc92ec2fc4
Elson <elson921121@gmail.com>
2025-04-28 05:11:33 +0800

    Initial commit - setup EatMove React App

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+function LoginPage() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <input type="text" placeholder="Email" /><br />
+          <input type="password" placeholder="Password" /><br />
+          <button>Login</button>
+        </form>
+      </div>
+    );
+  }
+  
+  export default LoginPage;
+  
---
+++ src/pages/UserProfilePage.jsx
@@ -0,0 +1,11 @@
+function UserProfilePage() {
+    return (
+      <div>
+        <h1>User Profile</h1>
+        <p>個人資訊、歷史訂單、EatCoin 餘額</p>
+      </div>
+    );
+  }
+  
+  export default UserProfilePage;
+  
